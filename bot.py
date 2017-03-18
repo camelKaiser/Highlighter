72895f3ef50eb7e4a9194dd6fa817f9809f20578
Winston Li <wzli98@gmail.com>
2017-03-18 16:17:28 -0400

    Added fix for unrecognizable characters

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import socket
 import time
 import re
+from datetime import datetime
 
 HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "jackalapples"
 PASS = "oauth:ab0krnwwqxyonvg6d0cn5g1vattcgt"
-CHAN = "#ogaminglol"
-log_file = open("log.txt", "w")
+CHAN = "#smashstudios"                          #change as needed
+log_file = open("fullLog2.txt", "w")
 
 
 s = socket.socket()
@@ -16,10 +17,11 @@ s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
 s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
 s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
 
-while True:
+while True:                                                 #data collection
     response = s.recv(1024).decode("utf-8")
-    if response == "PING :tmi.twitch.tv\r\n":
+    if response == "PING :tmi.twitch.tv\r\n":               #pong as needed
         s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
     else:
-        print(response)
-        log_file.write(response)
+        response = str(datetime.now()) + " " + response;    #attach timestamp
+        print(response.encode("utf-8"))                     #encode to handle invalid chars and write to file
+        log_file.write(response.encode("utf-8"));
